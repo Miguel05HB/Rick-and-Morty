5054b39b15bd126d7764ecb8edd22110cfeee917
Miguel05HB <76132931@certus.edu.pe>
2023-10-01 13:26:11 -0500

    API CREADA

---
+++ src/components/Character.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Character({ character }) {
+  return (
+    <div className="text-center p-5">
+      <h3>{character.name}</h3>
+      <img
+        className="img-fluid rounded-pill"
+        src={character.image}
+        alt={character.name}
+      />
+    </div>
+  );
+}
+
+export default Character;
